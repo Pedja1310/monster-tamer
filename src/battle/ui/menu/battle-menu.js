3289ed161e0fbdee9154f07baa40f425fc1c9875
Pedja Stojiljkovic <pedja1310@gmail.com>
2024-05-22 06:11:41 +0200

    finished refacotring of battle scene menu

---
+++ src/battle/ui/menu/battle-menu.js
@@ -0,0 +1,109 @@
+import { MONSTER_ASSET_KEYS } from "../../../asstes/asset-keys.js";
+
+const BATTLE_MENU_OPTIOS = Object.freeze({
+	FIGHT: "FIGHT",
+	SWITCH: "SWITCH",
+	ITEM: "ITEM",
+	FLEE: "FLEE",
+});
+
+const BATTLE_UI_TEXT_STYLES = {
+	color: "#000",
+	fontSize: "30px",
+};
+
+export class BattleMenu {
+	#scene;
+	#mainBattleMenuPhaserContainerGameObject;
+	#moveSelectionSubBattleMenuPhaserContainerGameObject;
+	#battleTextGameObjectLine1;
+	#battleTextGameObjectLine2;
+
+	constructor(scene) {
+		this.#scene = scene;
+
+		this.#createMainInfoPane();
+		this.#createMainBattleMenu();
+		this.#createMonsterAttackSubMenu();
+	}
+
+	showMainBattleMenu() {
+		this.#battleTextGameObjectLine1.setText("what should");
+		this.#mainBattleMenuPhaserContainerGameObject.setAlpha(1);
+		this.#battleTextGameObjectLine1.setAlpha(1);
+		this.#battleTextGameObjectLine2.setAlpha(1);
+	}
+
+	hideMainBattleMenu() {
+		this.#mainBattleMenuPhaserContainerGameObject.setAlpha(0);
+		this.#battleTextGameObjectLine1.setAlpha(0);
+		this.#battleTextGameObjectLine2.setAlpha(0);
+	}
+
+	showMonsterAttackSubMenu() {
+		this.#moveSelectionSubBattleMenuPhaserContainerGameObject.setAlpha(1);
+	}
+
+	hideMonsterAttackSubMenu() {
+		this.#moveSelectionSubBattleMenuPhaserContainerGameObject.setAlpha(0);
+	}
+
+	#createMainBattleMenu() {
+		this.#battleTextGameObjectLine1 = this.#scene.add.text(20, 460, "what should", BATTLE_UI_TEXT_STYLES);
+		//TODO: update to use monster data that is passed to this class
+		this.#battleTextGameObjectLine2 = this.#scene.add.text(
+			20,
+			512,
+			`${MONSTER_ASSET_KEYS.IGUANIGNITE} do next?`,
+			BATTLE_UI_TEXT_STYLES
+		);
+
+		this.#mainBattleMenuPhaserContainerGameObject = this.#scene.add.container(520, 448, [
+			this.#createMainSubInfoPane(),
+			this.#scene.add.text(55, 22, BATTLE_MENU_OPTIOS.FIGHT, BATTLE_UI_TEXT_STYLES),
+			this.#scene.add.text(240, 22, BATTLE_MENU_OPTIOS.SWITCH, BATTLE_UI_TEXT_STYLES),
+			this.#scene.add.text(55, 70, BATTLE_MENU_OPTIOS.ITEM, BATTLE_UI_TEXT_STYLES),
+			this.#scene.add.text(240, 70, BATTLE_MENU_OPTIOS.FLEE, BATTLE_UI_TEXT_STYLES),
+		]);
+
+		this.hideMainBattleMenu();
+	}
+
+	#createMonsterAttackSubMenu() {
+		this.#moveSelectionSubBattleMenuPhaserContainerGameObject = this.#scene.add.container(0, 448, [
+			this.#scene.add.text(55, 22, "slash", BATTLE_UI_TEXT_STYLES),
+			this.#scene.add.text(240, 22, "grawl", BATTLE_UI_TEXT_STYLES),
+			this.#scene.add.text(55, 70, "-", BATTLE_UI_TEXT_STYLES),
+			this.#scene.add.text(240, 70, "-", BATTLE_UI_TEXT_STYLES),
+		]);
+
+		this.hideMonsterAttackSubMenu();
+	}
+
+	#createMainInfoPane() {
+		const padding = 4;
+		const rectHeight = 124;
+
+		this.#scene.add
+			.rectangle(
+				0,
+				this.#scene.scale.height - rectHeight - padding,
+				this.#scene.scale.width - padding * 2,
+				rectHeight,
+				0xede4f3,
+				1
+			)
+			.setOrigin(0)
+			.setStrokeStyle(8, 0xe4434a, 1);
+	}
+
+	#createMainSubInfoPane() {
+		const rectWidth = 500;
+		const rectHeight = 124;
+
+		return this.#scene.add
+			.rectangle(0, 0, rectWidth, rectHeight, 0xede4f3, 1)
+			.setOrigin(0)
+			.setStrokeStyle(8, 0x905ac2, 1);
+	}
+}
--- src/scenes/battle-scene.js
+++ src/scenes/battle-scene.js
@@ -4,22 +4,13 @@ import {
 	HEALTH_BAR_ASSET_KEYS,
 	MONSTER_ASSET_KEYS,
 } from "../asstes/asset-keys.js";
+import { BattleMenu } from "../battle/ui/menu/battle-menu.js";
 import Phaser from "../lib/phaser.js";
 import { SCENE_KEYS } from "./scene-keys.js";
 
-const BATTLE_MENU_OPTIOS = Object.freeze({
-	FIGHT: "FIGHT",
-	SWITCH: "SWITCH",
-	ITEM: "ITEM",
-	FLEE: "FLEE",
-});
-
-const BATTLE_UI_TEXT_STYLES = {
-	color: "#000",
-	fontSize: "30px",
-};
-
 export class BattleScene extends Phaser.Scene {
+	#battleMenu;
+
 	constructor() {
 		super({ key: SCENE_KEYS.BATTLE_SCENE });
 	}
@@ -35,20 +26,13 @@ export class BattleScene extends Phaser.Scene {
 		this.add.image(256, 316, MONSTER_ASSET_KEYS.IGUANIGNITE, 0).setFlipX(true);
 
 		// render health bar
-		const playerMonsterName = this.add.text(
-			30,
-			20,
-			MONSTER_ASSET_KEYS.IGUANIGNITE,
-			{
-				color: "#7e3d3f",
-				fontSize: "32px",
-			}
-		);
+		const playerMonsterName = this.add.text(30, 20, MONSTER_ASSET_KEYS.IGUANIGNITE, {
+			color: "#7e3d3f",
+			fontSize: "32px",
+		});
 
 		this.add.container(556, 318, [
-			this.add
-				.image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
-				.setOrigin(0),
+			this.add.image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND).setOrigin(0),
 			playerMonsterName,
 			this.#createHealth(34, 34),
 			this.add.text(playerMonsterName.width + 35, 23, "L5", {
@@ -69,21 +53,13 @@ export class BattleScene extends Phaser.Scene {
 		]);
 
 		// render health bar
-		const enemyMonssterName = this.add.text(
-			30,
-			20,
-			MONSTER_ASSET_KEYS.CARNODUSK,
-			{
-				color: "#7e3d3f",
-				fontSize: "32px",
-			}
-		);
+		const enemyMonssterName = this.add.text(30, 20, MONSTER_ASSET_KEYS.CARNODUSK, {
+			color: "#7e3d3f",
+			fontSize: "32px",
+		});
 
 		this.add.container(0, 0, [
-			this.add
-				.image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
-				.setOrigin(0)
-				.setScale(1, 0.8),
+			this.add.image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND).setOrigin(0).setScale(1, 0.8),
 			enemyMonssterName,
 			this.#createHealth(34, 34),
 			this.add.text(enemyMonssterName.width + 35, 23, "L5", {
@@ -98,30 +74,14 @@ export class BattleScene extends Phaser.Scene {
 		]);
 
 		// render out the main info and sub info panes
-		this.#createMainInfoPane();
-		this.add.container(520, 448, [
-			this.#createMainSubInfoPane(),
-			this.add.text(55, 22, BATTLE_MENU_OPTIOS.FIGHT, BATTLE_UI_TEXT_STYLES),
-			this.add.text(240, 22, BATTLE_MENU_OPTIOS.SWITCH, BATTLE_UI_TEXT_STYLES),
-			this.add.text(55, 70, BATTLE_MENU_OPTIOS.ITEM, BATTLE_UI_TEXT_STYLES),
-			this.add.text(240, 70, BATTLE_MENU_OPTIOS.FLEE, BATTLE_UI_TEXT_STYLES),
-		]);
-
-		this.add.container(0, 448, [
-			this.add.text(55, 22, "slash", BATTLE_UI_TEXT_STYLES),
-			this.add.text(240, 22, "grawl", BATTLE_UI_TEXT_STYLES),
-			this.add.text(55, 70, "-", BATTLE_UI_TEXT_STYLES),
-			this.add.text(240, 70, "-", BATTLE_UI_TEXT_STYLES),
-		]);
+		this.#battleMenu = new BattleMenu(this);
+		this.#battleMenu.showMainBattleMenu();
 	}
 
 	#createHealth(x, y) {
 		const scaleY = 0.7;
 
-		const leftCap = this.add
-			.image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP)
-			.setOrigin(0, 0.5)
-			.setScale(1, scaleY);
+		const leftCap = this.add.image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP).setOrigin(0, 0.5).setScale(1, scaleY);
 
 		const middle = this.add
 			.image(leftCap.x + leftCap.width, y, HEALTH_BAR_ASSET_KEYS.MIDDLE)
@@ -135,32 +95,5 @@ export class BattleScene extends Phaser.Scene {
 			.setScale(1, scaleY);
 
 		return this.add.container(x, y, [leftCap, middle, rightCap]);
-	}
-
-	#createMainInfoPane() {
-		const padding = 4;
-		const rectHeight = 124;
-
-		this.add
-			.rectangle(
-				0,
-				this.scale.height - rectHeight - padding,
-				this.scale.width - padding * 2,
-				rectHeight,
-				0xede4f3,
-				1
-			)
-			.setOrigin(0)
-			.setStrokeStyle(8, 0xe4434a, 1);
-	}
-
-	#createMainSubInfoPane() {
-		const rectWidth = 500;
-		const rectHeight = 124;
-
-		return this.add
-			.rectangle(0, 0, rectWidth, rectHeight, 0xede4f3, 1)
-			.setOrigin(0)
-			.setStrokeStyle(8, 0x905ac2, 1);
 	}
 }
